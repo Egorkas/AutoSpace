7fc8457f1a301092d90f036853f72adb591b7970
Egorkas <egorkaras1997@gmail.com>
2022-07-05 23:25:35 +0300

    Add models Store and Product

---
+++ Store.Domain/Store.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Domain
+{
+    public class Store
+    {
+        public Guid StoreId { get; set; }
+        public string StoreName { get; set; }
+        public string StoreAddress { get; set; }
+        public DateTime OpeningTime { get; set; }
+        public DateTime ClosingTime { get; set; }
+        public IList<Product> Products { get; set; }
+    }
+}
