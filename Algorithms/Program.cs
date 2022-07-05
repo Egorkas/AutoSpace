0275ed7d10112456aa310b02a6b2a5f4a01c6bbd
Egorkas <egorkaras1997@gmail.com>
2022-07-05 18:04:26 +0300

    Add AddingEvenNumers

---
+++ Algorithms/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Algorithms.Services;
+
+var array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 11, 12 };
+var firstService = new FirstService();
+var result = firstService.AddEvenNumbers(array);
+Console.WriteLine(result);
