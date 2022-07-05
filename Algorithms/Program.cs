600a365982857d11d54ba8fa34e5f8e5f32e6106
Egorkas <egorkaras1997@gmail.com>
2022-07-05 19:33:56 +0300

    add removeOddElements

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Algorithms.Services;
+﻿using Algorithms.Services;
 
 //First Service
 var array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 11, 12 };
@@ -11,3 +10,16 @@ Console.WriteLine(result);
 var text = "hello wolrld !!!";
 var secondService = new SecondService();
 Console.WriteLine(secondService.RemoveRepeatedChars(text));//helo_wrd!
+
+//Third Service
+var list = new List<string>
+{
+    "1", "dsd", "2","sam","3",
+    "month", "4", "all", "5"
+};
+var thirdService = new ThirdService<string>();
+var resultList = thirdService.RemoveOddElements(list);//dsd sam month all
+foreach (var item in resultList)
+{
+    Console.WriteLine(item);
+}
---
+++ Algorithms/Services/ThirdService.cs
@@ -0,0 +1,10 @@
+﻿namespace Algorithms.Services
+{
+    public class ThirdService<T>
+    {
+        public List<T> RemoveOddElements(List<T> list)
+        {
+            return list.Where(v => list.IndexOf(v) % 2 == 1).ToList();
+        }
+    }
+}
