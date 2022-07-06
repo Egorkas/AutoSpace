b8435584597f5883b5e59180899b2d7fc66f0c25
Egorkas <egorkaras1997@gmail.com>
2022-07-06 23:34:52 +0300

    Add Persistense DI

---
+++ Store.Persistence/DependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Store.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Persistence
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            var connectionString = configuration["DbConnection"];
+            services.AddDbContext<StoreDbContext>(options =>
+            {
+                options.UseSqlServer(connectionString);
+            });
+            services.AddScoped<IStoreDbContext>(provider =>
+                provider.GetService<StoreDbContext>());
+            return services;
+        } 
+    }
+}
