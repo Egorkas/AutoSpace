9dfbbbaeff2044635cc41b0058afc5b923994739
Egorkas <egorkaras1997@gmail.com>
2022-07-06 18:55:28 +0300

    Add query for Product and Shop

---
+++ Store.Application/Products/GetProductByShopIdQuery.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Store.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Application.Products
+{
+    public class GetProductByShopIdQuery : IRequest<List<ProductDto>>
+    {
+        public Guid ShopId { get; set; }
+    }
+
+    public class GetProductByShopIdQueryHandler : IRequestHandler<GetProductByShopIdQuery, List<ProductDto>>
+    {
+        private readonly IStoreDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public GetProductByShopIdQueryHandler(IStoreDbContext dbContext, IMapper mapper) =>
+            (_dbContext, _mapper) = (dbContext, mapper);
+
+        public Task<List<ProductDto>> Handle(GetProductByShopIdQuery request, CancellationToken cancellationToken)
+        {
+            var productQuery = _dbContext.Products
+                .Where(item => item.Shops
+                    .Any(o => o.ShopId == request.ShopId))
+                .ProjectTo<ProductDto>(_mapper.ConfigurationProvider)
+                .ToListAsync(cancellationToken);
+
+            return productQuery;
+        }
+    }
+}
---
+++ Store.Application/Shops/Queries/ShopDto.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Store.Application.Common.Mappings;
+using Store.Domain;
+
+namespace Store.Application.Shops.Queries
+{
+    public class ShopDto : IMapWith<Shop>
+    {
+        public Guid ShopId { get; set; }
+        public string ShopName { get; set; }
+        public string ShopAddress { get; set; }
+        public TimeOnly OpeningTime { get; set; }
+        public TimeOnly ClosingTime { get; set; }
+        //public IList<Product> Products { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            //profile.CreateMap<Shop, ShopDto>().ReverseMap();
+            profile.CreateMap<Shop, ShopDto>()
+                .ForMember(shopDto => shopDto.ShopId,
+                    opt => opt.MapFrom(shop => shop.ShopId))
+                .ForMember(shopDto => shopDto.ShopName,
+                    opt => opt.MapFrom(shop => shop.ShopName))
+                .ForMember(shopDto => shopDto.ShopAddress,
+                    opt => opt.MapFrom(shop => shop.ShopAddress))
+                .ForMember(shopDto => shopDto.OpeningTime,
+                    opt => opt.MapFrom(shop => shop.OpeningTime))
+                .ForMember(shopDto => shopDto.ClosingTime,
+                    opt => opt.MapFrom(shop => shop.ClosingTime));
+        }
+    }
+}
