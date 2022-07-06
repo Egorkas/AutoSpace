f1308f02267f6a6954a4e04e6046ada4094508e8
Egorkas <egorkaras1997@gmail.com>
2022-07-07 00:02:22 +0300

    Add DI for Application

--- Store.Persistence/DependencyInjection.cs
+++ Store.Persistence/DependencyInjection.cs
@@ -2,11 +2,6 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Store.Application.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Store.Persistence
 {
--- Store.Persistence/StoreDbContext.cs
+++ Store.Persistence/StoreDbContext.cs
@@ -2,11 +2,6 @@
 using Store.Application.Interfaces;
 using Store.Domain;
 using Store.Persistence.EntityTypeConfigurations;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Store.Persistence
 {
