8f3320722800473d1b39d6d88f3ed2febade4ad6
Egorkas <egorkaras1997@gmail.com>
2022-07-08 00:59:03 +0300

    Create Controllers

---
+++ Store.Web/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Store.Application.Products;
+
+namespace Store.Web.Controllers
+{
+    public class ProductController : BaseController
+    {
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProductsByShopId(Guid id)
+        {
+            var query = new GetProductByShopIdQuery
+            {
+                ShopId = id
+            };
+            var products = await Mediator.Send(query);
+            return View(products);
+        }
+    }
+}
--- Store.Web/Program.cs
+++ Store.Web/Program.cs
@@ -1,15 +1,42 @@
+using Store.Application;
+using Store.Application.Common.Mappings;
+using Store.Application.Interfaces;
+using Store.Persistence;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
+
 // Add services to the container.
+builder.Services.AddAutoMapper(config =>
+{
+    config.AddProfile(new AssemblyMappingProfile(Assembly.GetExecutingAssembly()));
+    config.AddProfile(new AssemblyMappingProfile(typeof(IStoreDbContext).Assembly));
+});
+builder.Services.AddApplication();
+builder.Services.AddPersistence(builder.Configuration);
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+using(var scope = app.Services.CreateScope())
+{
+    var serviceProvider = scope.ServiceProvider;
+    try
+    {
+        var context = serviceProvider.GetRequiredService<StoreDbContext>();
+        DbInitializer.Initialize(context);
+    }
+    catch(Exception exception)
+    {
+
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -18,10 +45,12 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-app.UseAuthorization();
+//app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}");
 
 app.Run();
+
+
