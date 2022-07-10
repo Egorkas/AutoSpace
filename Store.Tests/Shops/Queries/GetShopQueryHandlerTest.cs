a3f7e47ce95b3be77765e7240e8fa7ea9890f58b
Egorkas <egorkaras1997@gmail.com>
2022-07-10 16:01:59 +0300

    Add unit Test for ShopsGetALL

---
+++ Store.Tests/Shops/Queries/GetShopQueryHandlerTest.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using Shouldly;
+using Store.Application.Shops.Queries;
+using Store.Persistence;
+using Store.Tests.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Tests.Shops.Queries
+{
+    [Collection("QueryCollection")]
+    public class GetShopQueryHandlerTest
+    {
+        private readonly StoreDbContext Context;
+        private readonly IMapper Mapper;
+        public GetShopQueryHandlerTest(QueryTestFixture fixture)
+        {
+            Context = fixture.Context;
+            Mapper = fixture.Mapper;
+        }
+        [Fact]
+        public async Task GetShopQueryHandler_Success()
+        {
+            //Arrange
+            var handler = new GetShopQueryHandler(Context, Mapper);
+            
+            //Act
+            var result = await handler.Handle(
+                new GetShopQuery(),
+                CancellationToken.None
+                );
+
+            //Assert
+            result.ShouldBeOfType<List<ShopDto>>();
+            result.Count.ShouldBe(1);
+        }
+    }
+}
---
+++ forddelete/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace forddelete
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
