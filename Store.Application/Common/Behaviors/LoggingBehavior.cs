29c931a708f6499dbbc9c0c38b96ec31eeee5082
Egorkas <egorkaras1997@gmail.com>
2022-07-10 20:35:08 +0300

    Add Config For Serilog

---
+++ Store.Application/Common/Behaviors/LoggingBehavior.cs
@@ -0,0 +1,25 @@
+﻿using Serilog;
+using MediatR;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Application.Common.Behaviors
+{
+    public class LoggingBehavior<Trequest, TResponce>
+        : IPipelineBehavior<Trequest, TResponce> where Trequest
+        : IRequest<TResponce>
+    {
+        public async Task<TResponce> Handle(Trequest request,
+            CancellationToken cancellationToken,
+            RequestHandlerDelegate<TResponce> next)
+        {
+            var requestName = typeof(Trequest).Name;
+            Log.Information("Notes Request: {Name}  {@Request}",
+                requestName, request);
+
+            var responce = await next();
+
+            return responce;
+        }
+    }
+}
